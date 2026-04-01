b025a69d7ad59694e3a84b7b04d3e18d22036006
dhensley0991 <donald.r.hensley@gmail.com>
2026-04-01 01:04:27 -0400

    fix login route

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -21,7 +21,7 @@ export default function LoginPage() {
       body.append("username", subscriptionId);
       body.append("password", password);
 
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/login`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
