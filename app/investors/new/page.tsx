16789fc91ee8101e4debf6c733c7332b9b40fd2a
dhensley0991 <donald.r.hensley@gmail.com>
2026-04-02 21:58:49 -0400

    new endpoint

--- app/investors/new/page.tsx
+++ app/investors/new/page.tsx
@@ -47,6 +47,7 @@ export default function AddInvestorPage() {
 
   useEffect(() => {
     const token = localStorage.getItem("artemis_token");
+    console.log("TOKEN:", token);
 
     if (!token) {
       router.push("/login");
